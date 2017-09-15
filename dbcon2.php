c236193046087e895ffbecffe3ac88460445056c
adamvertes <31598669+adamvertes@users.noreply.github.com>
2017-09-15 14:03:04 +0200

    Update dbcon2.php

--- dbcon2.php
+++ dbcon2.php
@@ -4,10 +4,7 @@ const HOSTNAME  = 'localhost'; // server
 const MYSQLUSER = 'root'; // database user
 const MYSQLPASS = 'root'; // database password
 const MYSQLDB   = 'mydb'; // database name
-/*const HOSTNAME  = 'marianacph.dk.mysql'; // server 
-const MYSQLUSER = 'marianacph_dk'; // database user
-const MYSQLPASS = 'vhKqxUzP'; // database password
-const MYSQLDB   = 'marianacph_dk'; // database name*/
+
 
 // Create database connection object
 $con = new mysqli(HOSTNAME, MYSQLUSER, MYSQLPASS, MYSQLDB);
